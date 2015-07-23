be1bf476e41d3ba1075d9153dc0e0ec30b250e11
marginTop <djmax256081@gmail.com>
2015-07-23 11:05:31 +0800

    add comment header

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var riot = require('gulp-riot');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var minifyCSS = require('gulp-minify-css');
-var uglify = require('gulp-uglify'); 
+var uglify = require('gulp-uglify');
+var header = require('gulp-header');
+var pkg = require('./package.json');
 
 gulp.task('riot_pc', function () {
     return gulp.src(['src/pc/*.tag'])
@@ -27,6 +29,7 @@ gulp.task('riot_light', function () {
 });
 
 gulp.task('build', function () {
+    
     gulp.src(['src/css/themes/*.css'])
     .pipe(gulp.dest('build/themes'))
     .pipe(minifyCSS())
@@ -38,10 +41,27 @@ gulp.task('build', function () {
     .pipe(gulp.dest('build'))
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
+    .pipe(setHeader())
     .pipe(gulp.dest('build'));
 });
 
 gulp.task('default', ['riot_pc', 'riot_mobile', 'build'], function () {
     gulp.watch(['src/*/*.tag', 'src/*.tag', 'src/themes/*.css'], ['riot_pc', 'riot_mobile', 'build']);
 });
+
+function setHeader() {
+    var date = new Date();
+    date = date.toDateString();
+
+    var banner = [
+        '/**',
+        ' * <%= pkg.name %> v<%= pkg.version %>',
+        ' * Revised in <%= date %>',
+        ' * Released under the <%= pkg.license %> License.',
+        ' */',
+        '\n'
+    ].join('\n');
+
+    return header(banner, {pkg: pkg, date: date});
+}
 
