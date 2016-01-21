cb101b2ade04bb3997d999fccbd5f549714df0ff
leiquan <leiquan@baidu.com>
2016-01-21 14:32:54 +0800

    上传组件简单测试

--- build/iToolkit_pc.js
+++ build/iToolkit_pc.js
@@ -3277,98 +3277,53 @@ riot.tag('itk-tree', '<div class="tree-item-wrap" each="{ item, i in data }" ons
     
     
 });
-riot.tag('itk-uploader', '<div class="container"> <div class="page-header"> <h1>Simple Ajax Uploader</h1> <h3>Basic Example</h3> </div> <div class="row" style="padding-top:10px;"> <div class="col-xs-2"> <button id="uploadBtn" class="btn btn-large btn-primary">Choose File</button> </div> <div class="col-xs-10"> <div id="progressOuter" class="progress progress-striped active" style="display:none;"> <div id="progressBar" class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 0%" </div> </div> </div> </div> <div class="row" style="padding-top:10px;"> <div class="col-xs-10"> <div id="msgBox"></div> </div> </div> </div>', function(opts) {
+riot.tag('itk-uploader', '<div class="btn btn-large btn-primary" name="uploadBtn" id="uploadBtn">上传</div>', '#progressOuter { border: 1px solid #ccc; border-radius: 4px; height: 32px; margin: 0; background: white; border-top-right-radius: 0; border-bottom-right-radius: 0; display: none; }', function(opts) {
         var self = this;
         var EL = self.root;
         var config = self.opts.opts || self.opts;
 
+        var randomNumber = function (min, max) {
+            return Math.floor(min + Math.random() * (max - min));
+        };
+        self['uploadBtn'].id = randomNumber(10000, 99999);
+
         var js = document.scripts;
-
-        var path = '';
-
         var jsPath = '';
-
-        if (!config.path) {
-            for (var i = 0; i < js.length; i++) {
-                if (!js[i].src) {
-                    continue;
-                }
-                if (/iToolkit_pc.min.js|iToolkit_pc.js/.test(js[i].src)) {
-                    jsPath = js[i].src.replace(/iToolkit_pc.min.js|iToolkit_pc.js/, '');
-                    break;
-                }
+        for (var i = 0; i < js.length; i++) {
+            if (!js[i].src) {
+                continue;
             }
-            path = jsPath + 'plugins/uploader/';
+            if (/iToolkit_pc.min.js|iToolkit_pc.js/.test(js[i].src)) {
+                jsPath = js[i].src.replace(/iToolkit_pc.min.js|iToolkit_pc.js/, '');
+                break;
+            }
         }
-        else {
-            path = config.path;
-        }
-
-
+        path = jsPath + 'plugins/uploader/';
         var sourceArr = [
             path + 'SimpleAjaxUploader.min.js',
         ];
 
         utils.jsLoader(sourceArr, function () {
-            var btn = document.getElementById('uploadBtn'),
-                    progressBar = document.getElementById('progressBar'),
-                    progressOuter = document.getElementById('progressOuter'),
-                    msgBox = document.getElementById('msgBox');
 
-            var uploader = new ss.SimpleUpload({
-                button: btn,
-                url: 'file_upload.php',
-                name: 'uploadfile',
-                multipart: true,
-                hoverClass: 'hover',
-                focusClass: 'focus',
-                responseType: 'json',
-                startXHR: function () {
-                    progressOuter.style.display = 'block'; // make progress bar visible
-                    this.setProgressBar(progressBar);
-                },
-                onSubmit: function () {
-                    msgBox.innerHTML = ''; // empty the message box
-                    btn.innerHTML = 'Uploading...'; // change button text to "Uploading..."
-                },
-                onComplete: function (filename, response) {
-                    btn.innerHTML = 'Choose Another File';
-                    progressOuter.style.display = 'none'; // hide progress bar when upload is completed
+            var btn = document.getElementById(self['uploadBtn'].id);
 
-                    if (!response) {
-                        msgBox.innerHTML = 'Unable to upload file';
-                        return;
-                    }
+            console.log(btn);
 
-                    if (response.success === true) {
-                        msgBox.innerHTML = '<strong>' + escapeTags(filename) + '</strong>' + ' successfully uploaded.';
+            var json ={};
+            json.button = btn;
+            json.url = config.url;
+            json.name = config.name ? config.name : "";
+            json.multipart = config.multipart ? config.multipart : true;
+            json.responseType = config.responseType ? config.responseType : "";
+            json.startXHR = config.startXHR ? config.startXHR : null;
+            json.onSubmit = config.onSubmit ? config.onSubmit : null;
+            json.onComplete = config.onComplete ? config.onComplete : null;
+            json.onError = config.onError ? config.onError : null;
 
-                    } else {
-                        if (response.msg) {
-                            msgBox.innerHTML = escapeTags(response.msg);
 
-                        } else {
-                            msgBox.innerHTML = 'An error occurred and the upload failed.';
-                        }
-                    }
-                },
-                onError: function () {
-                    progressOuter.style.display = 'none';
-                    msgBox.innerHTML = 'Unable to upload file';
-                }
-<<<<<<< HEAD
-            },
-            onError: function() {
-                progressOuter.style.display = 'none';
-                msgBox.innerHTML = 'Unable to upload file';
-            } 
-        });        
-    });
-=======
-            });
+
+            var uploader = new ss.SimpleUpload(json);
         });
-
->>>>>>> 3abf7fbfbd76ae2668fa3212ff2f3acdc5eaf1c4
 
     
 });
--- src/pc/itk-uploader.tag
+++ src/pc/itk-uploader.tag
@@ -1,39 +1,40 @@
 <itk-uploader>
-    <div class="container">
-        <div class="page-header">
-            <h1>Simple Ajax Uploader</h1>
-            <h3>Basic Example</h3>
-        </div>
-        <div class="row" style="padding-top:10px;">
-            <div class="col-xs-2">
-              <button id="uploadBtn" class="btn btn-large btn-primary">Choose File</button>
-            </div>
-            <div class="col-xs-10">
-                <div id="progressOuter" class="progress progress-striped active" style="display:none;">
-                    <div id="progressBar" class="progress-bar progress-bar-success"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 0%"</div>
-                </div>
-            </div>
-        </div>
-        <div class="row" style="padding-top:10px;">
-            <div class="col-xs-10">
-                <div id="msgBox"></div>
-            </div>
-        </div>
-    </div>
 
+    <style>
+
+        #progressOuter {
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            height: 32px;
+            margin: 0;
+            background: white;
+            border-top-right-radius: 0;
+            border-bottom-right-radius: 0;
+            display: none;
+        }
+
+    </style>
+
+
+    <div class="btn btn-large btn-primary" name="uploadBtn" id="uploadBtn">上传</div>
 
     <script>
-    var self = this;
-    var EL = self.root;
-    var config = self.opts.opts || self.opts;
 
-    var js = document.scripts;
+        // 初始化设置
+        var self = this;
+        var EL = self.root;
+        var config = self.opts.opts || self.opts;
 
-    var path = '';
+        // 通过随机数,构造 btn ID, 防止重复.
+        var randomNumber = function (min, max) {
+            return Math.floor(min + Math.random() * (max - min));
+        };
+        self['uploadBtn'].id = randomNumber(10000, 99999);
 
-    var jsPath = '';
 
-    if (!config.path) {
+        // 获取脚本路径,load 组件
+        var js = document.scripts;
+        var jsPath = '';
         for (var i = 0; i < js.length; i++) {
             if (!js[i].src) {
                 continue;
@@ -44,65 +45,35 @@
             }
         }
         path = jsPath + 'plugins/uploader/';
-    }
-    else {
-        path = config.path;
-    }
+        var sourceArr = [
+            path + 'SimpleAjaxUploader.min.js',
+        ];
 
-    var sourceArr = [
-        path + 'SimpleAjaxUploader.min.js',
-    ];
+        // 调用组件
+        utils.jsLoader(sourceArr, function () {
 
-    utils.jsLoader(sourceArr, function () {
-        var btn = document.getElementById('uploadBtn'),
-            progressBar = document.getElementById('progressBar'),
-            progressOuter = document.getElementById('progressOuter'),
-            msgBox = document.getElementById('msgBox');
-        //..............
-        var uploader = new ss.SimpleUpload({
-            button: btn,
-            url: '/src/plugins/uploader/examples/basic_example/file_upload.php',
-            name: 'uploadfile',
-            multipart: true,
-            hoverClass: 'hover',
-            focusClass: 'focus',
-            responseType: 'json',
-            startXHR: function() {
-                progressOuter.style.display = 'block'; // make progress bar visible
-                this.setProgressBar( progressBar );
-            },
-            onSubmit: function() {
-                msgBox.innerHTML = ''; // empty the message box
-                btn.innerHTML = 'Uploading...'; // change button text to "Uploading..."
-            },
-            onComplete: function( filename, response ) {
-                btn.innerHTML = 'Choose Another File';
-                progressOuter.style.display = 'none'; // hide progress bar when upload is completed
-                if ( !response ) {
-                    msgBox.innerHTML = 'Unable to upload file';
-                    return;
-                }
+            var btn = document.getElementById(self['uploadBtn'].id);
 
-                if ( response.success === true ) {
-                    msgBox.innerHTML = '<strong>' + filename + '</strong>' + ' successfully uploaded.';
+            console.log(btn);
 
-                } else {
-                    if ( response.msg )  {
-                        msgBox.innerHTML = response.msg ;
+            // 更新设置
+            var json = {};
+            json.button = btn;
 
-                    } else {
-                        msgBox.innerHTML = 'An error occurred and the upload failed.';
-                    }
-                }
-            },
-            onError: function() {
-                progressOuter.style.display = 'none';
-                msgBox.innerHTML = 'Unable to upload file';
-            }
-        });        
-    });
+            // 这些选项来源于配置
+            json.url = config.url;
+            json.name = config.name ? config.name : "";
+            json.multipart = config.multipart ? config.multipart : true;
+            json.responseType = config.responseType ? config.responseType : "";
+            json.startXHR = config.startXHR ? config.startXHR : null;
+            json.onSubmit = config.onSubmit ? config.onSubmit : null;
+            json.onComplete = config.onComplete ? config.onComplete : null;
+            json.onError = config.onError ? config.onError : null;
 
-    
+
+            var uploader = new ss.SimpleUpload(json);
+        });
+
     </script>
 
 </itk-uploader>
