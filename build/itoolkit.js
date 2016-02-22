a9039522788971ed149f50c1fb089a745e89f42a
leiquan <leiquan@live.com>
2016-02-22 20:22:53 +0800

    the slider has been cached and hidden until the dependencies are loaded

--- build/itoolkit.js
+++ build/itoolkit.js
@@ -3458,63 +3458,77 @@ riot.tag('itk-select', '<yield></yield> <ul class="itk-selected-container" onmou
 
 riot.tag('itk-slide', ' <yield>', function(opts) {
 
-        var self = this;
-        var EL = self.root;
-        var config = self.opts.opts || self.opts;
-        var js = document.scripts;
-        var path = '';
-        var jsPath = '';
+            var self = this;
+            var EL = self.root;
+            var config = self.opts.opts || self.opts;
+            var js = document.scripts;
+            var path = '';
+            var jsPath = '';
 
 
-        for (var i = 0; i < js.length; i++) {
-            if (!js[i].src) {
-                continue;
+            for (var i = 0; i < js.length; i++) {
+                if (!js[i].src) {
+                    continue;
+                }
+                if (/itoolkit.min.js|itoolkit.js/.test(js[i].src)) {
+                    jsPath = js[i].src.replace(/itoolkit.min.js|itoolkit.js/, '');
+                    break;
+                }
             }
-            if (/itoolkit.min.js|itoolkit.js/.test(js[i].src)) {
-                jsPath = js[i].src.replace(/itoolkit.min.js|itoolkit.js/, '');
-                break;
-            }
-        }
 
-        path = jsPath + 'plugins/';
+            path = jsPath + 'plugins/';
 
-        if (typeof jQuery == 'undefined') {
-            (function () {
-                utils.jsLoader([
-                    path + 'jquery/jquery-1.12.0.min.js',
-                ], function () {
+            var elArr = [];
 
-                    jQuery(document).ready(function ($) {
-                        utils.jsLoader([
-                            path + 'slick/slick.css',
-                            path + 'slick/slick-theme.css',
-                            path + 'slick/slick.js',
-                        ], function () {
-                            $(document).ready(function () {
-                                $(EL).slick(config);
+            if (typeof jQuery == 'undefined') {
+                (function () {
+                    EL.style.visibility = 'hidden';
+                    elArr.push(EL);
+                    utils.jsLoader([
+                        path + 'jquery/jquery-1.12.0.min.js',
+                    ], function () {
+
+                        jQuery(document).ready(function ($) {
+                            utils.jsLoader([
+                                path + 'slick/slick.css',
+                                path + 'slick/slick-theme.css',
+                                path + 'slick/slick.js',
+                            ], function () {
+                                $(document).ready(function () {
+
+                                    for (var i = 0; i < elArr.length; i++) {
+                                        elArr[i].style.visibility = 'visible';
+                                    }
+
+                                    $(EL).slick(config);
+                                });
                             });
                         });
                     });
-                });
-            })();
-        } else {
-            jQuery(document).ready(function ($) {
-                utils.jsLoader([
-                    path + 'slick/slick.css',
-                    path + 'slick/slick-theme.css',
-                    path + 'slick/slick.js'
-                ], function () {
-                    $(document).ready(function () {
-                        $(EL).slick(config);
+                })();
+            } else {
+                jQuery(document).ready(function ($) {
+                    EL.style.visibility = 'hidden';
+                    elArr.push(EL);
+                    utils.jsLoader([
+                        path + 'slick/slick.css',
+                        path + 'slick/slick-theme.css',
+                        path + 'slick/slick.js'
+                    ], function () {
+                        $(document).ready(function () {
+                            for (var i = 0; i < elArr.length; i++) {
+                                elArr[i].style.visibility = 'visible';
+                            }
+                            $(EL).slick(config);
+                        });
                     });
                 });
-            });
-        }
+            }
 
-        self.on('mount', function() {
-            self.root.style.display = 'block';
-        })
-    
+            self.on('mount', function () {
+                self.root.style.display = 'block';
+            })
+        
 });
 riot.tag('itk-table', '<yield>', function(opts) {
         var self = this;
--- src/pc/itk-slide.tag
+++ src/pc/itk-slide.tag
@@ -4,64 +4,77 @@
 
     <yield>
 
-    <script>
+        <script>
 
-        var self = this;
-        var EL = self.root;
-        var config = self.opts.opts || self.opts;
-        var js = document.scripts;
-        var path = '';
-        var jsPath = '';
+            var self = this;
+            var EL = self.root;
+            var config = self.opts.opts || self.opts;
+            var js = document.scripts;
+            var path = '';
+            var jsPath = '';
 
 
-        for (var i = 0; i < js.length; i++) {
-            if (!js[i].src) {
-                continue;
+            for (var i = 0; i < js.length; i++) {
+                if (!js[i].src) {
+                    continue;
+                }
+                if (/itoolkit.min.js|itoolkit.js/.test(js[i].src)) {
+                    jsPath = js[i].src.replace(/itoolkit.min.js|itoolkit.js/, '');
+                    break;
+                }
             }
-            if (/itoolkit.min.js|itoolkit.js/.test(js[i].src)) {
-                jsPath = js[i].src.replace(/itoolkit.min.js|itoolkit.js/, '');
-                break;
-            }
-        }
 
-        path = jsPath + 'plugins/';
+            path = jsPath + 'plugins/';
 
-        if (typeof jQuery == 'undefined') {
-            (function () {
-                utils.jsLoader([
-                    path + 'jquery/jquery-1.12.0.min.js',
-                ], function () {
-                    // $.noConflict();
-                    jQuery(document).ready(function ($) {
-                        utils.jsLoader([
-                            path + 'slick/slick.css',
-                            path + 'slick/slick-theme.css',
-                            path + 'slick/slick.js',
-                        ], function () {
-                            $(document).ready(function () {
-                                $(EL).slick(config);
+            // 异步调用,所以元素要先缓存起来,否则不是一致的
+            var elArr = [];
+
+            if (typeof jQuery == 'undefined') {
+                (function () {
+                    EL.style.visibility = 'hidden';
+                    elArr.push(EL);
+                    utils.jsLoader([
+                        path + 'jquery/jquery-1.12.0.min.js',
+                    ], function () {
+                        // $.noConflict();
+                        jQuery(document).ready(function ($) {
+                            utils.jsLoader([
+                                path + 'slick/slick.css',
+                                path + 'slick/slick-theme.css',
+                                path + 'slick/slick.js',
+                            ], function () {
+                                $(document).ready(function () {
+                                    for (var i = 0; i < elArr.length; i++) {
+                                        elArr[i].style.visibility = 'visible';
+                                    }
+                                    $(EL).slick(config);
+                                });
                             });
                         });
                     });
-                });
-            })();
-        } else {
-            jQuery(document).ready(function ($) {
-                utils.jsLoader([
-                    path + 'slick/slick.css',
-                    path + 'slick/slick-theme.css',
-                    path + 'slick/slick.js'
-                ], function () {
-                    $(document).ready(function () {
-                        $(EL).slick(config);
+                })();
+            } else {
+                jQuery(document).ready(function ($) {
+                    EL.style.visibility = 'hidden';
+                    elArr.push(EL);
+                    utils.jsLoader([
+                        path + 'slick/slick.css',
+                        path + 'slick/slick-theme.css',
+                        path + 'slick/slick.js'
+                    ], function () {
+                        $(document).ready(function () {
+                            for (var i = 0; i < elArr.length; i++) {
+                                elArr[i].style.visibility = 'visible';
+                            }
+                            $(EL).slick(config);
+                        });
                     });
                 });
-            });
-        }
+            }
 
-        self.on('mount', function() {
-            self.root.style.display = 'block';
-        })
-    </script>
+            self.on('mount', function () {
+                self.root.style.display = 'block';
+            })
+        </script>
 
 </itk-slide>
