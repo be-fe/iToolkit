301d08fb5bfd5b716ab851ce75b3cdcb833e0203
xieyu33333 <xieyu33333@gmail.com>
2016-08-15 17:42:10 +0800

    get-start添加postmessage

--- demos/js/js-code.js
+++ demos/js/js-code.js
@@ -1,17 +1,23 @@
 (function() {
     var demoArea = document.getElementById('demo-script');
-    var sourceCodeArea = document.createElement('pre');
-    sourceCodeArea.innerHTML = '<code class="language-javascript">' + demoArea.innerHTML + '<\/code>';
-    if (document.querySelector('.container')) {
-        document.querySelector('.container').appendChild(sourceCodeArea)
-    }
-    else {
-        document.body.appendChild(sourceCodeArea);
+    if (demoArea) {
+        var sourceCodeArea = document.createElement('pre');
+
+        sourceCodeArea.innerHTML = '<code class="language-javascript">' + demoArea.innerHTML + '<\/code>';
+        if (document.querySelector('.container')) {
+            document.querySelector('.container').appendChild(sourceCodeArea)
+        }
+        else {
+            document.body.appendChild(sourceCodeArea);
+        }
     }
 
-    setTimeout(function() {
-        var height = parseInt(window.getComputedStyle(document.body).height) + 40;
-        window.parent.postMessage(height, '*');
-    }, 100);
+    if (window.postMessage) {
+        setTimeout(function() {
+            var height = parseInt(window.getComputedStyle(document.body).height) + 40;
+            console.log(height);
+            window.parent.postMessage(height, '*');
+        }, 100);
+    }
     
 })();
